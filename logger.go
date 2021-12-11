895dd251fb19392072aef8d39298e387d88cbd47
lianyue <admin@lianyue.org>
2021-12-12 00:19:44 +0800

    v0.1.0

---
+++ logger.go
@@ -0,0 +1,48 @@
+package applogger
+
+import (
+	"os"
+
+	appconfig "github.com/otamoe/app-config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+func init() {
+	core := Core(nil)
+	Logger = zap.New(core)
+}
+
+func SetLogger(logger *zap.Logger) {
+	Logger = logger
+}
+
+func GetLogger() *zap.Logger {
+	return Logger
+}
+
+func Sync() error {
+	return Logger.Sync()
+}
+
+func Core(core zapcore.Core) zapcore.Core {
+	if core == nil {
+		encoderCfg := zapcore.EncoderConfig{
+			MessageKey:     "msg",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.RFC3339TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+		}
+
+		if appconfig.GetString("env") == "development" {
+			core = zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg), os.Stdout, zap.DebugLevel)
+		} else {
+			core = zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), os.Stdout, zap.InfoLevel)
+		}
+	}
+	return core
+}
